94687c04e160325ff37d068d4ecce3946b61f255
Adarsh1415 <164674483+Adarsh1415@users.noreply.github.com>
2025-08-03 18:12:54 +0530

    Create SpamDetector.java

---
+++ SpamDetector.java
@@ -0,0 +1,18 @@
+package service;
+
+import model.Email;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpamDetector {
+private static final List<String> SPAM_KEYWORDS = Arrays.asList("win", "free", "prize", "click here");
+
+public void detect(Email email) {
+for (String word : SPAM_KEYWORDS) {
+if (email.getContent().toLowerCase().contains(word)) {
+email.markAsSpam();
+break;
+}
+}
+}
+}
